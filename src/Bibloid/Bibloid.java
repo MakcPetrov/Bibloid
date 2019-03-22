928c14bca5d4600760a8da5a42be705178d9e6f0
MakcPetrov <k-113@yandex.ru>
2019-03-22 14:42:04 +0300

    login ok

--- src/Bibloid/Bibloid.java
+++ src/Bibloid/Bibloid.java
@@ -4,7 +4,7 @@ public class Bibloid {
 
     public static void main(String[] args) {
         //База
-        User user = new User(DataMan.getInstance());//создать экземпляр пользака
-        new MainWin(user,DataMan.getInstance());//создать окно
+        User user = new User(DataMan.getInstance());//создать экземпляр пользака и дать ему доступ к данным
+        new MainWin(user,DataMan.getInstance());//создать окно и дать ему доступ к данным
     }
 }
